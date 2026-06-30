8149642dc7686e62301f469ff43dc5242cb2227f
Raja Sampath DEA <s.rajasekaran94@gmail.com>
2026-06-30 15:51:16 +0530

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE dbo.shop(shop_id Integer , shop_name varchar(100), shop_location varchar(100));
+CREATE TABLE IF NOT EXISTS dbo.shop(shop_id Integer , shop_name varchar(100), shop_location varchar(100));
