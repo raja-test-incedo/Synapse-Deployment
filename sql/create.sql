3d9029e3a6d64f386ed67bc4522ec5d02ff56b25
Raja Sampath DEA <s.rajasekaran94@gmail.com>
2026-06-30 15:57:19 +0530

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,8 @@
-CREATE TABLE IF NOT EXISTS dbo.shop(shop_id Integer , shop_name varchar(100), shop_location varchar(100));
+IF OBJECT_ID('dbo.shop', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.shop (
+        shop_id INT,
+        shop_name VARCHAR(100),
+        shop_location VARCHAR(100)
+    );
+END
